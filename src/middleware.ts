ff96403e15c889a92628d8e6c5ad36ccc87590d7
kir4che <mollydcxxiii@gmail.com>
2025-08-15 21:08:13 +0800

    fix: correct middleware placement

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  if (process.env.NODE_ENV !== 'development') return NextResponse.redirect(new URL('/', req.url));
+  const nodeEnv = process.env.NODE_ENV || 'production';
+
+  if (nodeEnv !== 'development') return NextResponse.redirect(new URL('/', req.url));
 
   return NextResponse.next();
 }
